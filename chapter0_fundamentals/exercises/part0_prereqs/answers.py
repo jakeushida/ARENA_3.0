e4a27319a2455ae1c4e90bf5fb6ce2c2aa464f48
Jake Ushida <jakeushida@gmail.com>
2026-05-02 13:08:58 +1000

    finished C2-G of '# Exercises - einops operations & broadcasting'

--- chapter0_fundamentals/exercises/part0_prereqs/answers.py
+++ chapter0_fundamentals/exercises/part0_prereqs/answers.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import einops
 import numpy as np
 import torch as t
+from eindex import eindex
 from torch import Tensor
 from utils import display_array_as_img
 
@@ -141,4 +142,134 @@ def normalize_rows(matrix: Tensor) -> Tensor:
 matrix = t.tensor([[1, 2, 3], [4, 5, 6], [7, 8, 9]]).float()
 expected = t.tensor([[0.267, 0.535, 0.802], [0.456, 0.570, 0.684], [0.503, 0.574, 0.646]])
 assert_all_close(normalize_rows(matrix), expected)
+# %% (C2) pairwise cosine similarity
+def cos_sim_matrix(matrix: Tensor) -> Tensor:
+    """Return the cosine similarity matrix for each pair of rows of the given matrix.
+
+    matrix: shape (m, n)
+    """
+    normalized_matrix = normalize_rows(matrix)
+    return normalized_matrix @ normalized_matrix.T
+    raise NotImplementedError()
+
+
+matrix = t.tensor([[1, 2, 3], [4, 5, 6], [7, 8, 9]]).float()
+expected = t.tensor([[1.0, 0.975, 0.959], [0.975, 1.0, 0.998], [0.959, 0.998, 1.0]])
+assert_all_close(cos_sim_matrix(matrix), expected)
+# %% (D) sample distribution
+def sample_distribution(probs: Tensor, n: int) -> Tensor:
+    """Return n random samples from probs, where probs is a normalized probability distribution.
+
+    probs: shape (k,) where probs[i] is the probability of event i occurring.
+    n: number of random samples
+
+    Return: shape (n,) where out[i] is an integer indicating which event was sampled.
+
+    Use t.rand and t.cumsum to do this without any explicit loops.
+    """
+    return (t.rand(n, 1) > probs.cumsum(dim=0)).sum(dim=1)
+    raise NotImplementedError()
+
+
+n = 5_000_000
+probs = t.tensor([0.05, 0.1, 0.1, 0.2, 0.15, 0.4])
+freqs = t.bincount(sample_distribution(probs, n)) / n
+assert_all_close(freqs, probs)
+# %% (E) classifier accuracy
+def classifier_accuracy(scores: Tensor, true_classes: Tensor) -> Tensor:
+    """Return the fraction of inputs for which the maximum score corresponds to the true class for that input.
+
+    scores: shape (batch, n_classes). A higher score[b, i] means that the classifier thinks class i is more likely.
+    true_classes: shape (batch, ). true_classes[b] is an integer from [0...n_classes).
+
+    Use t.argmax.
+    """
+    return (scores.argmax(dim=1) == true_classes).sum() / len(true_classes)
+    raise NotImplementedError()
+
+
+scores = t.tensor([[0.75, 0.5, 0.25], [0.1, 0.5, 0.4], [0.1, 0.7, 0.2]])
+true_classes = t.tensor([0, 1, 0])
+expected = 2.0 / 3.0
+assert classifier_accuracy(scores, true_classes) == expected
+print("Tests passed!")
+# %% (F1) total price indexing
+def total_price_indexing(prices: Tensor, items: Tensor) -> float:
+    """Given prices for each kind of item and a tensor of items purchased, return the total price.
+
+    prices: shape (k, ). prices[i] is the price of the ith item.
+    items: shape (n, ). A 1D tensor where each value is an item index from [0..k).
+
+    Use integer array indexing. The below document describes this for NumPy but it's the same in PyTorch:
+
+    https://numpy.org/doc/stable/user/basics.indexing.html#integer-array-indexing
+    """
+    # return prices.gather(0, items).sum()
+    return eindex(prices, items, '[i]').sum()
+    raise NotImplementedError()
+
+
+prices = t.tensor([0.5, 1, 1.5, 2, 2.5])
+items = t.tensor([0, 0, 1, 1, 4, 3, 2])
+assert total_price_indexing(prices, items) == 9.0
+print("Tests passed!")
+# %% (F2) gather 2D
+def gather_2d(matrix: Tensor, indexes: Tensor) -> Tensor:
+    """Perform a gather operation along the second dimension.
+
+    matrix: shape (m, n)
+    indexes: shape (m, k)
+
+    Return: shape (m, k). out[i][j] = matrix[i][indexes[i][j]]
+
+    For this problem, the test already passes and it's your job to write at least three asserts relating the arguments and the output. This is a tricky function and worth spending some time to wrap your head around its behavior.
+
+    See: https://pytorch.org/docs/stable/generated/torch.gather.html?highlight=gather#torch.gather
+    """
+    # YOUR CODE HERE - add assert statement(s) here for `indices` and `matrix`
+    assert matrix.size(0) >= indexes.size(0)
+    assert matrix.dim() == indexes.dim()
+
+    # out = matrix.gather(1, indexes)
+    out = eindex(matrix, indexes, 'row [row col]')
+    
+    # YOUR CODE HERE - add assert statement(s) here for `out`
+    assert out.shape == indexes.shape
+
+    return out
+
+
+matrix = t.arange(15).view(3, 5)
+indexes = t.tensor([[4], [3], [2]])
+expected = t.tensor([[4], [8], [12]])
+assert_all_equal(gather_2d(matrix, indexes), expected)
+
+indexes2 = t.tensor([[2, 4], [1, 3], [0, 2]])
+expected2 = t.tensor([[2, 4], [6, 8], [10, 12]])
+assert_all_equal(gather_2d(matrix, indexes2), expected2)
+# %% (G) indexing
+def integer_array_indexing(matrix: Tensor, coords: Tensor) -> Tensor:
+    """Return the values at each coordinate using integer array indexing.
+
+    For details on integer array indexing, see:
+    https://numpy.org/doc/stable/user/basics.indexing.html#integer-array-indexing
+
+    matrix: shape (d_0, d_1, ..., d_n)
+    coords: shape (batch, n)
+
+    Return: (batch, )
+    """
+    return matrix[tuple(coords.T)]
+    raise NotImplementedError()
+
+
+mat_2d = t.arange(15).view(3, 5)
+coords_2d = t.tensor([[0, 1], [0, 4], [1, 4]])
+actual = integer_array_indexing(mat_2d, coords_2d)
+assert_all_equal(actual, t.tensor([1, 4, 9]))
+
+mat_3d = t.arange(2 * 3 * 4).view((2, 3, 4))
+coords_3d = t.tensor([[0, 0, 0], [0, 1, 1], [0, 2, 2], [1, 0, 3], [1, 2, 0]])
+actual = integer_array_indexing(mat_3d, coords_3d)
+assert_all_equal(actual, t.tensor([0, 5, 10, 15, 20]))
 # %%
