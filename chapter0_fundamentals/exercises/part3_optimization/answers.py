5cbbe0ca7fc083a6af7da1b3a9f6285443c04587
Jake Ushida <jakeushida@gmail.com>
2026-06-01 20:53:14 +1000

    finished "Exercise - implement `opt_fn_with_sgd`"

---
+++ chapter0_fundamentals/exercises/part3_optimization/answers.py
@@ -0,0 +1,83 @@
+# %% 
+import sys
+from pathlib import Path
+from typing import Callable, Iterable, Literal
+
+import torch as t
+from jaxtyping import Float
+from torch import Tensor, optim
+
+# Make sure exercises are in the path
+chapter = "chapter0_fundamentals"
+section = "part3_optimization"
+root_dir = next(p for p in Path.cwd().parents if (p / chapter).exists())
+exercises_dir = root_dir / chapter / "exercises"
+section_dir = exercises_dir / section
+if str(exercises_dir) not in sys.path:
+    sys.path.append(str(exercises_dir))
+
+
+import part3_optimization.tests as tests
+from part2_cnns.solutions import Linear, ResNet34, get_resnet_for_feature_extraction
+from part3_optimization.utils import plot_fn, plot_fn_with_points
+from plotly_utils import bar, imshow, line
+
+device = t.device("mps" if t.backends.mps.is_available() else "cuda" if t.cuda.is_available() else "cpu")
+# %%
+def pathological_curve_loss(x: Tensor, y: Tensor):
+    # Example of a pathological curvature. There are many more possible, feel free to experiment here!
+    x_loss = t.tanh(x) ** 2 + 0.01 * t.abs(x)
+    y_loss = t.sigmoid(y)
+    return x_loss + y_loss
+
+
+plot_fn(pathological_curve_loss, min_points=[(0, "y_min")])
+
+# %%
+# Exercise - implement `opt_fn_with_sgd`
+def opt_fn_with_sgd(
+    fn: Callable, xy: Float[Tensor, "2"], lr=0.001, momentum=0.98, n_iters: int = 100
+) -> Float[Tensor, "n_iters 2"]:
+    """
+    Optimize the a given function starting from the specified point.
+
+    xy: shape (2,). The (x, y) starting point.
+    n_iters: number of steps.
+    lr, momentum: parameters passed to the torch.optim.SGD optimizer.
+
+    Return: (n_iters+1, 2). The (x, y) values, from initial values to values after step `n_iters`.
+    """
+    # Make sure tensor has requires_grad=True, otherwise it can't be optimized (more on this tomorrow!)
+    assert xy.requires_grad
+
+    optimizer = optim.SGD(params=(xy,), lr=lr, momentum=momentum)
+
+    xy_list = [xy.detach().clone()]
+
+    for _ in range(n_iters):
+        loss = fn(xy[0], xy[1])
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        xy_list.append(xy.detach().clone())
+
+    return t.stack(xy_list, dim=0)
+    raise NotImplementedError()
+
+
+points = []
+
+optimizer_list = [
+    (optim.SGD, {"lr": 0.1, "momentum": 0.0}),
+    (optim.SGD, {"lr": 0.02, "momentum": 0.99}),
+]
+
+for optimizer_class, params in optimizer_list:
+    xy = t.tensor([2.5, 2.5], requires_grad=True)
+    xys = opt_fn_with_sgd(pathological_curve_loss, xy=xy, lr=params["lr"], momentum=params["momentum"])
+    points.append((xys, optimizer_class, params))
+    print(f"{params=}, last point={xys[-1]}")
+
+plot_fn_with_points(pathological_curve_loss, points=points, min_points=[(0, "y_min")])
+# %%
